d1591450d7773920bfb1d1dfc921998b0b2e1222
jomion <jeremihovos@gmail.com>
2026-09-24 19:17:50 +0100

    Add files via upload

---
+++ pages/eleve/cahier-ecriture/js/main.mjs
@@ -0,0 +1,15 @@
+// Seyes est un logiciel libre
+// sous licence GNU GPL
+// écrit par Arnaud Champollion
+// Il fait partie de la suite Éducajou
+
+import "../css/styles.css";
+import { initMenu } from "./core/menu/menu.mjs";
+import { initDialoguePdf } from "./core/pdf/index.mjs";
+import { initSettings } from "./core/settings/initSettings.mjs";
+import { initEvents } from "./core/ui/events/init-events.mjs";
+
+initSettings();
+initMenu();
+initEvents();
+initDialoguePdf();
